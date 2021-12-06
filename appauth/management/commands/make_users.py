3e0c0c64b8625af96b2cc4cde337bc619e65f6ca
stopguard <stopguard@bk.ru>
2021-12-06 20:36:01 +0500

    other small fixes

--- appauth/management/commands/make_users.py
+++ appauth/management/commands/make_users.py
@@ -12,10 +12,10 @@ class Command(BaseCommand):
             first_name = f'name {i:0>2}'
             last_name = f'surname {i:0>2}'
             if not get_user_model().objects.filter(Q(username=username) | Q(email=email)).exists():
-                get_user_model().objects.create_superuser(username, email, f'password{email}',
-                                                          first_name=first_name, last_name=last_name)
+                get_user_model().objects.create_user(username, email, f'password{email}',
+                                                     first_name=first_name, last_name=last_name)
                 counter += 1
                 print(f'User {username} created')
             else:
                 print(f'User {username} already is exists, or email {email} is already exists')
-            print(f'{counter} user created')
+        print(f'{counter} users created')
